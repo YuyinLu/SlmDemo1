523e006b8790d2ea84482a33e913aa336c718aa0
雨茵 陆 <yinlu@yuyindeMacBook-Air.local>
2023-06-12 17:28:26 +0200

    Initial commit

---
+++ src/main/java/com/example/slmdemo/SlmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.slmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slmdemo/SlmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
